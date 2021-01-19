57d8d7a94548828add6a6aeff637ddc51bc8b9c9
gracehoward <gkhoward@ucla.edu>
2021-01-18 23:01:36 -0800

    wip: install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,36 +1,71 @@
 #!/usr/bin/env bash
 
-# 1. Change default shell to zsh
-# chsh -s $(which zsh)
+sudo -v
 
-# Install packages via pacman/yay
+DOTS="$HOME/dotfiles/"
 
-# Make directories if DNE
-# mkdir ~/.config/nvim
+print_usage() {
+cat << EOM
+USAGE:
+    ./install.sh [OPTIONS]
+OPTIONS:
+    -h Print this message
+EOM
+}
 
-# Install oh-my-zsh
-# Maybe have to chmod +x run scripts???
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+install_packages() {
+    bash ./packages.sh -i
+}
 
-# Create symlinks
-# ~.i3/config
-# ~/.config/kitty/kitty.conf
-# ~/.config/nvim/init.vim
-# ~/.config/rofi/slate.rasi
-# ~/.zshrc
+setup_zsh() {
+    # Install oh-my-zsh
+    # Maybe have to chmod +x run scripts???
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Polybar
-# Install fonts globally(?)
-# Move fonts to /usr/share/fonts (system-wide) or ~/.local/share/fonts/ (single user)
-# Or download from AUR
-# sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
-# fc-cache
+    # Change default shell to zsh
+    chsh -s $(which zsh)
+}
 
-# Neovim extras
-# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+create_symlinks() {
+    # TODO: Make directories if DNE
+    # mkdir ~/.config/nvim
 
-# main() {
-# }
+    # TODO: Check if file already exists
+    ln -si "$DOTS/i3/config" "$HOME/.i3/config"
+    ln -si "$DOTS/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+    ln -si "$DOTS/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+    ln -si "$DOTS/rofi/slate.rasi" "$HOME/.config/rofi/slate.rasi"
+    ln -si "$DOTS/polybar/" "$HOME/.config/"
 
-# main "$@"
+    ln -si "$DOTS/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+    ln -si "$DOTS/nvim/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
+    ln -si "$DOTS/.vimrc" "$HOME/.vimrc"
+
+    ln -si "$DOTS/.zshrc" "$HOME/.zshrc"
+}
+
+setup_polybar() {
+    sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
+}
+
+install_neovim() {
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+main() {
+    # while getopts ":h" flag; do
+    #     case $flag in
+    #         h ) print_usage; return 0 ;;
+    #         ? ) print_usage; return 1 ;;
+    #     esac
+    # done
+    # shift $(( OPTIND - 1 ))
+
+    # install_packages
+    # setup_zsh
+    # setup_polybar
+    # create_symlinks
+}
+
+main "$@"
