3de33de0dce892b52f66f8de605899ecb98577fc
grcekh <gracekimhoward@gmail.com>
2023-07-15 23:55:59 -0700

    updates to nvim plugins config

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -19,7 +19,7 @@ require("nvim-treesitter.install").compilers = { "gcc-11" }
 
 require("nvim-treesitter.configs").setup({
   ensure_installed = { "bash", "c", "cpp", "css", "dart", "glsl", "go", "graphql", "html", "java", "javascript", "jsdoc",
-    "json", "latex", "lua", "make", "norg", "php", "python", "regex", "ruby", "rust", "scss", "svelte", "tsx",
+    "json", "latex", "lua", "make", "nix", "norg", "php", "python", "regex", "ruby", "rust", "scss", "svelte", "tsx",
     "typescript", "vim" },
   highlight = {
     enable = true
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -15,26 +15,31 @@ return require("packer").startup(function()
   use "wbthomason/packer.nvim"
 
   -- Core
-  use {
-    "kyazdani42/nvim-tree.lua",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function() require("config/nvim-tree") end,
-  }
-  use { "neoclide/coc.nvim", branch = "release" }
-  use "tpope/vim-commentary"
-  use "tpope/vim-fugitive"
-  use "glepnir/dashboard-nvim"
   use "romgrk/barbar.nvim"
+  use { "neoclide/coc.nvim", branch = "release" }
+  use "glepnir/dashboard-nvim"
+  -- use "folke/flash.nvim"
   use {
     "glepnir/galaxyline.nvim",
     branch = "main",
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function() require("config/galaxyline") end,
   }
+  -- use {
+  --   "ggandor/leap.nvim",
+  --   config = function() require("leap").add_default_mappings() end,
+  -- }
   use {
     "nvim-telescope/telescope.nvim", tag = "0.1.0",
     requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } }
   }
+  use {
+    "kyazdani42/nvim-tree.lua",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function() require("config/nvim-tree") end,
+  }
+  use "tpope/vim-commentary"
+  use "tpope/vim-fugitive"
 
   -- Syntax
   use "tikhomirov/vim-glsl"
@@ -55,7 +60,7 @@ return require("packer").startup(function()
     "norcalli/nvim-colorizer.lua",
     config = function()
       require("colorizer").setup(
-        { "*"; },
+        { "*", },
         { RRGGBBAA = true, css = true }
       )
     end
